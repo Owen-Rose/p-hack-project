ad8a2a71c0a6731835d764e0c78976a97438c6af
Quynh Ngo <QuynhTNgo@live.com>
2022-12-15 12:14:24 -0700

    Create readFile method for csv file

--- probability_statistics_crime.py
+++ probability_statistics_crime.py
@@ -1,4 +1,12 @@
+import csv
+
 class CrimeStatistics():
+    crime_statistics_list = []
+    #Relevant data should be in the list above, so sector, community name, category, crime count and year from the provided csv file
+    def readFile(self):
+        with open("Community_Crime_Statistics.csv") as csvfile:
+            csvRead = csv.reader(csvfile)
+            
     pass
 
 class CrimeProbability():
@@ -9,3 +17,6 @@ class CrimeProbability():
         def yearly_trend(year):
             pass            
 
+x = CrimeStatistics()
+
+x.readFile()
