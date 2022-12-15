8a897c48e363ea488c6e7919a2a06b0d39ef708f
Quynh Ngo <QuynhTNgo@live.com>
2022-12-15 12:20:07 -0700

    readFile in statistics class now reads for relevant data

--- probability_statistics_crime.py
+++ probability_statistics_crime.py
@@ -6,7 +6,10 @@ class CrimeStatistics():
     def readFile(self):
         with open("Community_Crime_Statistics.csv") as csvfile:
             csvRead = csv.reader(csvfile)
-            
+            row_data = []
+            for element in csvRead:
+                row_data += [[element[0], element[1], element[2], element[3], element[6]]]
+                print(row_data)
     pass
 
 class CrimeProbability():
